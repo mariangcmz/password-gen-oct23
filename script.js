161bf3b002e06217301ac82191cfdc11cb11b660
mariangcmz <mariangcmz@gmail.com>
2023-10-09 18:33:26 -0600

    added event listener

--- script.js
+++ script.js
@@ -1,8 +1,21 @@
 // Assignment code here
-
-
+const types = {
+  upperCase: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+  lowerCase: "abcdefghijklmnopqrstuvwxyz",
+  numbers: "0123456789",
+  symbols: "!@#$%^&*()_+~\`|}{[]:;?><,./-="
+}
+var length 
+var selectu
+var selectl
+var selectn
+var selects
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); 
+generate.addEventListener("click", (writePassword)); {
+  var button = Event.target;
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -14,4 +27,6 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);{
+
+}
